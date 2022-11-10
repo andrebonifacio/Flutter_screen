9c1da8c40810100b5c52caf8fa1de9fbcd0148a0
andrebds <andre.junior@opah.com.br>
2022-11-10 15:09:33 -0300

    commit inicial

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plant_app/constants.dart';
+import 'package:plant_app/screens/home/components/header_with_searchbox.dart';
+import 'package:plant_app/screens/home/components/title_with_more_btn.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: "Recommended",
+            press: () {},
+          ),
+          RecommendedPlantCard(
+            image: "assets/images/image_1.png",
+            title: "Samantha",
+            country: "Russia",
+            price: 440,
+            press: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RecommendedPlantCard extends StatelessWidget {
+  const RecommendedPlantCard({
+    Key? key,
+    required this.image,
+    required this.title,
+    required this.country,
+    required this.price,
+    required this.press,
+  }) : super(key: key);
+
+  final String image, title, country;
+  final int price;
+  final Function() press;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding),
+      width: size.width * 0.4,
+      child: Column(
+        children: <Widget>[
+          Image.asset(image),
+          GestureDetector(
+            onTap: press,
+            child: Container(
+              padding: EdgeInsets.all(kDefaultPadding / 2),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10)),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 50,
+                    color: kPrimaryColor.withOpacity(0.23),
+                  ),
+                ],
+              ),
+              child: Row(
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                            text: "$title".toUpperCase(),
+                            style: Theme.of(context).textTheme.button),
+                        TextSpan(
+                          text: "$country".toUpperCase(),
+                          style: TextStyle(
+                            color: kPrimaryColor.withOpacity(
+                              0.5,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Spacer(),
+                  Text(
+                    '\$$price',
+                    style: Theme.of(context)
+                        .textTheme
+                        .button!
+                        .copyWith(color: kPrimaryColor),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
